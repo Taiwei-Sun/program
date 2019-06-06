ef61262927f20a23b624f459ce043767f3f803b0
root <root@perry.org>
2019-06-06 16:21:19 +0800

    catTrace test OK

--- C/diameterTest/catTrace.c
+++ C/diameterTest/catTrace.c
@@ -41,26 +41,51 @@ int main(int argc, char **argv)
 {
 
 
-  /*printf("argc=%d\n",argc);
+  //printf("argc=%d\n",argc);
   //printf("argv[0]=%s\n",argv[0]);
-  printf("argv[1]=%s\n",argv[1]);
+  //printf("argv[1]=%s\n",argv[1]);
 
+  int len;
+  char *packetS;
   if(argc!=2){
-    printf("only one parameter\n");
-    exit(1);
-  }*/
-
-  int len=strlen(TmpStr);
-  //int len=strlen(argv[1]);
+    char packetS1[1024];
+	scanf("%s",packetS1);
+	int pLen=strlen(packetS1);
+	printf("pLen=%d\n",pLen);
+	packetS=malloc(sizeof(char)*(pLen));
+	int i;
+	for(i=0;i<pLen;i++){
+		packetS[i]=packetS1[i];
+	}
+	
+	
+  }else{
+	  packetS=argv[1];
+	  
+  }
+  
+  len=strlen(packetS);
+  //len=strlen(TmpStr);
   int bufLen=(len/2)+(len&2);
-  printf("len=%d\nbufLen=%d\n",len,bufLen);
+  //printf("len=%d\nbufLen=%d\n",len,bufLen);
   char buf[bufLen];  
 
-  char2charV(TmpStr,buf);
+  char2charV(packetS,buf);
+  //char2charV(TmpStr,buf);
 
 
-
-  initAvpIndex(buf);
+  initDiameter(buf);
+  
+  printf("Diameter version=%d\n",diameterHeaderStruct.version);
+  printf("Diameter length=%d\n",diameterHeaderStruct.len);
+  printf("Diameter flags=%02x\n",diameterHeaderStruct.flags&0xff);
+  printf("Diameter command code=%x\n",diameterHeaderStruct.commandCode);
+  printf("Diameter application ID=%x\n",diameterHeaderStruct.applicationID);
+  printf("Diameter Hop By Hop ID=%x\n",diameterHeaderStruct.hopByHopID);
+  printf("Diameter End To End ID=%x\n",diameterHeaderStruct.endToEndID);
+  printf("\n");
+  
+  
   int i;
   for(i=0;i<avpIndexNum;i++){
    char tmpS[512];
@@ -72,47 +97,9 @@ int main(int argc, char **argv)
    printf("len=%06x\n",avpIndex[i].len);
    getAvpDataStrByIndex(i,tmpS);
    printf("data=\"%s\"\n",tmpS);
-   printf("--------\n\n");
+   printf("--------\n");
   }
-/*
-  unsigned char buffer[BUFFER_SIZE];
-  int in_len=0;
-  int i;
 
-  client_len=0;
-
- 
-  printf("Start--hex\n");
-  for(i=0;i<in_len;i++)
-   printf("%02x",buffer[i]);
-
-  printf("\n---char\n");
-  for(i=0;i<in_len;i++)
-   if(buffer[i]>31&&buffer[i]<127)
-    printf("%c",buffer[i]);
-   else printf("X");
-  printf("\nEnd\n");
- 
-
-
-  int ttt=getDiameterHeaderNum(DiameterHeaderMessageLenth,buffer);
-  printf("message length=%d\n",ttt);
-  ttt=getDiameterHeaderNum(DiameterHeaderCommandCode,buffer);
-  printf("Command Code=%d\n",ttt);
-  ttt=getDiameterHeaderNum(DiameterHeaderApplicationID,buffer);
-  printf("Application ID=%d\n",ttt);
-  
-  ttt=findAvpIndexByStr("Origin-Host",buffer);
-  printf("Origin-Host index=%d\n",ttt);
-  ttt=findAvpIndexByStr("Origin-Realm",buffer);
-  printf("Origin-Realm index=%d\n",ttt);
-
-  char ccc[40];
-  int lll=getAvpDataStrByNum(buffer,ttt,ccc);
-  //printf("Origin-Host = %s\n",ccc);
-  printf("Origin-Realm = %s\n",ccc);
-
-*/
 
 
 
--- C/diameterTest/dictionary.h
+++ C/diameterTest/dictionary.h
@@ -30,7 +30,17 @@ int DiameterHeaderIndexListNum[DiameterHeaderMax]={0,1,4,5,8,12,16};
 char AvpListStr[AvpMax][AvpStrMax]={"Acct-Interim-Interval","Accounting-Realtime-Required","Acct-Multi-Session-Id","Accounting-Record-Number","Accounting-Record-Type","Acct-Session-Id","Accounting-Sub-Session-Id","Acct-Application-Id","Auth-Application-Id","Auth-Request-Type","Authorization-Lifetime","Auth-Grace-Period","Auth-Session-State","Re-Auth-Request-Type","Class","Destination-Host","Destination-Realm","Disconnect-Cause","Error-Message","Error-Reporting-Host","Event-Timestamp","Experimental-Result","Experimental-Result-Code","Failed-AVP","Firmware-Revision","Host-IP-Address","Inband-Security-Id","Multi-Round-Time-Out","Origin-Host","Origin-Realm","Origin-State-Id","Product-Name","Proxy-Host","Proxy-Info","Proxy-State","Redirect-Host","Redirect-Host-Usage","Redirect-Max-Cache-Time","Result-Code","Route-Record","Session-Id","Session-Timeout","Session-Binding","Session-Server-Failover","Supported-Vendor-Id","Termination-Cause","User-Name","Vendor-Id","Vendor-Specific-Application-Id"};
 int AvpListNum[AvpMax]={85,483,50,485,480,44,287,259,258,274,291,276,277,285,25,293,283,273,281,294,55,297,298,279,267,257,299,272,264,296,278,269,280,284,33,292,261,262,268,282,263,27,270,271,265,295,1,266,260};
 
-
+typedef struct DiameterHeader {
+  int version;
+  int len;
+  char flags;
+  int commandCode;
+  int applicationID;
+  int hopByHopID;
+  int endToEndID;
+} DiameterHeader;
+DiameterHeader diameterHeaderStruct;
+int diameterHeaderStructOK=-1;
 int DiameterVersion=0x01;
 int DiameterFlags=0x80;
 typedef struct AvpData {
@@ -211,8 +221,8 @@ void getAvpDataStrByIndex(int index,char *buf)
 int getAvpNum(char *packet)
 {
   int i=0;
-  int p=20;
-  int len=getDiameterHeaderNum(DiameterHeaderMessageLenth,packet);
+  int p=DiameterHeaderLenMax;
+  int len=diameterHeaderStruct.len;
   while(p<len){
 
    int aLen=getAvpHeaderNum(AvpHeaderLength,packet,p);
@@ -223,11 +233,35 @@ int getAvpNum(char *packet)
   return i;
 }
 
+void initDiameterHeader(char *packet)
+{
+	/*
+	int version;
+	int len;
+	char flags;
+	int commandCode;
+	int applicationID;
+	int hopByHopID;
+	int endToEndID;
+	*/
+	
+	diameterHeaderStruct.version=getDiameterHeaderNum(DiameterHeaderVersion,packet);
+	diameterHeaderStruct.len=getDiameterHeaderNum(DiameterHeaderMessageLenth,packet);
+	diameterHeaderStruct.flags=getDiameterHeaderNum(DiameterHeaderFlags,packet)&0xff;
+	diameterHeaderStruct.commandCode=getDiameterHeaderNum(DiameterHeaderCommandCode,packet);
+	diameterHeaderStruct.applicationID=getDiameterHeaderNum(DiameterHeaderApplicationID,packet);
+	diameterHeaderStruct.hopByHopID=getDiameterHeaderNum(DiameterHeaderHopByHop,packet);
+	diameterHeaderStruct.endToEndID=getDiameterHeaderNum(DiameterHeaderEndToEnd,packet);
+	diameterHeaderStructOK=1;
+}
 void initAvpIndex(char *packet)
 {
-	//printf("0\n");
-	int pos=20;
-	int len=getDiameterHeaderNum(DiameterHeaderMessageLenth,packet);
+	if(diameterHeaderStructOK!=1){
+		printf("init Diameter Header not yet\n");
+		return;
+	}
+	int pos=DiameterHeaderLenMax;
+	int len=diameterHeaderStruct.len;
 	//printf("1 len=%d\n",len);
 	avpIndexNum=getAvpNum(packet);
 	printf("avpIndexNum=%d\n",avpIndexNum);
@@ -256,6 +290,12 @@ void initAvpIndex(char *packet)
 	//printf("\n3\n");
 	
 }
+
+void initDiameter(char *packet){
+	initDiameterHeader(packet);
+	initAvpIndex(packet);
+}
+
 
 //---old
 int AcctInterimInterval=85;int AccountingRealtimeRequired=483;int AcctMultiSessionId=50;int AccountingRecordNumber=485;int AccountingRecordType=480;int AcctSessionId=44;int AccountingSubSessionId=287;int AcctApplicationId=259;int AuthApplicationId=258;int AuthRequestType=274;int AuthorizationLifetime=291;int AuthGracePeriod=276;int AuthSessionState=277;int ReAuthRequestType=285;int Class=25;int DestinationHost=293;int DestinationRealm=283;int DisconnectCause=273;int ErrorMessage=281;int ErrorReportingHost=294;int EventTimestamp=55;int ExperimentalResult=297;int ExperimentalResultCode=298;int FailedAVP=279;int FirmwareRevision=267;int HostIPAddress=257;int InbandSecurityId=299;int MultiRoundTimeOut=272;int OriginHost=264;int OriginRealm=296;int OriginStateId=278;int ProductName=269;int ProxyHost=280;int ProxyInfo=284;int ProxyState=33;int RedirectHost=292;int RedirectHostUsage=261;int RedirectMaxCacheTime=262;int ResultCode=268;int RouteRecord=282;int SessionId=263;int SessionTimeout=27;int SessionBinding=270;int SessionServerFailover=271;int SupportedVendorId=265;int TerminationCause=295;int UserName=1;int VendorId=266;int VendorSpecificApplicationId=260;
