6bdf2538e1e814db810c2105baa75582f6a14af0
Taiwei Sun <ghnbsun2011@gmail.com>
2020-04-07 07:24:21 +0000

    add python/test/* in .gitignore

---
+++ python/test/functionReturnTest.py
@@ -0,0 +1,32 @@
+# A Python program to generate squares from 1 
+# to 100 using yield and therefore generator 
+
+# An infinite generator function that prints 
+# next square number. It starts with 1 
+def nextSquare(): 
+    i = 1; 
+    yield i;
+    i = 1 +1;
+    yield i;
+	# An Infinite loop to generate squares 
+    #while True: 
+    #    yield i*i				 
+    #    i += 1 # Next execution resumes 
+				# from this point	 
+
+# Driver code to test above generator 
+# function 
+#for num in nextSquare(): 
+#    if num > 100: 
+#        break	
+#    print(num)
+
+
+#for num in nextSquare(): 
+#    print(num) 
+#    break	
+    
+
+num = nextSquare();
+num=list(num)
+print(num[0])
